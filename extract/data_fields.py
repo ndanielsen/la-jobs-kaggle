48de9d4228eb102e319a919932302b996c68b894
Nathan Danielsen <nathan.danielsen@gmail.com>
2019-05-13 21:13:12 -0700

    initial stuff

---
+++ extract/data_fields.py
@@ -0,0 +1,66 @@
+
+def get_file_name(*args):
+    # FILE_NAME
+
+    return ""
+
+def get_job_class_title(*args):
+    # JOB_CLASS_TITLE
+
+    return ""
+
+def get_job_class_no(*args):
+    # JOB_CLASS_NO
+    return ""
+
+def get_requirement_set_id(*args):
+    # REQUIREMENT_SET_ID
+    return ""
+
+
+def get_requirement_subset_id(*args):
+    # REQUIREMENT_SUBSET_ID
+
+    return ""
+
+def get_job_duties(*args):
+    # JOB_DUTIES
+    return ""
+
+
+def get_education_years(*args):
+    # EDUCATION_YEARS
+    return ""
+
+def get_school_type(*args):
+    # SCHOOL_TYPE
+    return ""
+
+
+def get_education_major(*args):
+    # EDUCATION_MAJOR
+    return ""
+
+def get_experience_length(*args):
+    # EXPERIENCE_LENGTH
+    return ""
+
+def get_full_time_part_time(*args):
+    # FULL_TIME_PART_TIME
+    return ""
+
+
+# EXP_JOB_CLASS_TITLE
+# EXP_JOB_CLASS_ALT_RESP
+# EXP_JOB_CLASS_FUNCTION
+# COURSE_COUNT
+# COURSE_LENGTH
+# COURSE_SUBJECT
+# MISC_COURSE_DETAILS
+# DRIVERS_LICENSE_REQ
+# DRIV_LIC_TYPE
+# ADDTL_LIC
+# EXAM_TYPE
+# ENTRY_SALARY_GEN
+# ENTRY_SALARY_DWP
+# OPEN_DATE
